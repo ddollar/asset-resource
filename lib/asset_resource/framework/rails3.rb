001ec2a8259fca96dccc369c4d5484a103fc9cc9
David Dollar <ddollar@gmail.com>
2011-01-10 23:34:08 -0500

    default rails3 middleware installation

--- lib/asset_resource/framework/rails3.rb
+++ lib/asset_resource/framework/rails3.rb
@@ -3,13 +3,14 @@ require "asset_resource/middleware"
 require "active_support"
 require 'asset_resource/framework/rails_view_helpers'
 
-# example usage: put this in your application.rb
-  # config.middleware.use AssetResource::Middleware,
-    # :base_path => Rails.root.join("public"),
-    # :handlers  => { :javascripts => "text/javascript",
-                    # :stylesheets => "text/css" }
-# end
-
+class AssetResource::Railtie < Rails::Railtie
+  config.before_configuration do
+    config.app_middleware.use AssetResource::Middleware,
+      :base_path => Rails.root.join("public"),
+      :handlers  => { :javascripts => "text/javascript",
+                      :stylesheets => "text/css" }
+  end
+end
 
 ActiveSupport.on_load(:action_view) do
   include AssetResource::Helpers
