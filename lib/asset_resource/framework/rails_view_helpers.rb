a8d69135375a06fa2205805d55d25b52d1d73c46
alex@liivid.com <alex@liivid.com>
2011-01-12 21:54:23 -0800

    Adding temporary cache buster (this is a bad solution just for me right now.  this plugin needs serious work)

--- lib/asset_resource/framework/rails_view_helpers.rb
+++ lib/asset_resource/framework/rails_view_helpers.rb
@@ -1,11 +1,11 @@
 module AssetResource
   module Helpers
     def asset_resource_stylesheets(resource_name = "stylesheets")
-      "<link rel='stylesheet' href='/assets/#{resource_name}.css' />".html_safe
+      "<link rel='stylesheet' href='/assets/#{resource_name}.css?#{Time.now.to_i}' />".html_safe
     end
 
     def asset_resource_javascripts(resource_name = "javascripts")
-      "<script type='text/javascript' src='/assets/#{resource_name}.js'></script>".html_safe
+      "<script type='text/javascript' src='/assets/#{resource_name}.js?#{Time.now.to_i}'></script>".html_safe
     end
   end
 end
